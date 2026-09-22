07d63913dcfab7ee45692b9969897112ac77855d
NocnyDominator <nocnydominator@gmail.com>
2026-09-22 18:25:54 +0200

    Update page.tsx

--- app/regulamin/page.tsx
+++ app/regulamin/page.tsx
@@ -7,7 +7,7 @@ export default function RegulaminPage() {
                 </h1>
 
                 <p className="text-[#666] font-medium mb-8">
-                    Z dnia: 23.04.2026
+                    Z dnia: 22.09.2026
                 </p>
 
                 <div className="p-8 md:p-12 leading-relaxed text-[#666]">
@@ -86,13 +86,13 @@ export default function RegulaminPage() {
                             </p>
                             <p>
                                 Adres: ul. Gałczyńskiego 5/15,
-                                <strong> [KOD POCZTOWY] [MIEJSCOWOŚĆ]</strong>
+                                <strong>12-200 Pisz</strong>
                             </p>
                             <p>
                                 E-mail: nocnydominator@gmail.com
                             </p>
                             <p>
-                                Telefon: <strong>[NUMER TELEFONU]</strong>
+                                Telefon: <strong>UZUPEŁNIĆ</strong>
                             </p>
                         </div>
 
